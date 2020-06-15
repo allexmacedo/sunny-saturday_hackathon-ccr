f4ea1c2456a7bfae2c0de0d61e3a071836cdd870
Lucas Antevere <lucas.antevere1901@gmail.com>
2020-06-14 21:05:04 -0300

    MockData

--- HackathonCCR/DAO/FacilitiesDAO.swift
+++ HackathonCCR/DAO/FacilitiesDAO.swift
@@ -73,16 +73,21 @@ final class FacilitiesDAO: NetworkDAO<FacilityAPI>, FacilitiesDataStore {
 
     func retriveFacilities(ofCategory category: FacilityCategory, completion: @escaping (Result<[CommercialFacility], Error>) -> Void) {
         
-        completion(.success([CommercialFacility(id: UUID(),
-                                                name: "Graal Paloma",
-                                                category: [FacilityCategory(id: nil, name: "Postos de combustivel", description: nil)],
-                                                description: "A Rede Graal é uma rede de auto-postos rodoviários do Brasil, sendo conhecida pelos seus amplos e modernos rodoshoppings. Está presente em algumas das principais rodovias nos estados de São Paulo, Minas Gerais, Rio de Janeiro, Santa Catarina e Rio Grande do Sul.",
-                                                address: " Rodovia Engenheiro João Baptista Cabral Rennó,s/n - Zona Rural, Santa Cruz do Rio Pardo - SP, 18900-000",
-                                                timeOfWork: "Aberto 24h",
-                                                phone: "(14) 3332-1033",
-                                                image: "graal",
-                                                rating: 4.5,
-                                                verified: true,
-                                                coordinate: CLLocation(latitude: -22.8061941, longitude: -49.531047))]))
+        switch category.id {
+            case 1, 2, 7:
+                completion(.success([CommercialFacility(id: UUID(),
+                                                        name: "Graal Paloma",
+                                                        category: [FacilityCategory(id: nil, name: "Postos de combustivel", description: nil)],
+                                                        description: "A Rede Graal é uma rede de auto-postos rodoviários do Brasil, sendo conhecida pelos seus amplos e modernos rodoshoppings. Está presente em algumas das principais rodovias nos estados de São Paulo, Minas Gerais, Rio de Janeiro, Santa Catarina e Rio Grande do Sul.",
+                                                        address: " Rodovia Engenheiro João Baptista Cabral Rennó,s/n - Zona Rural, Santa Cruz do Rio Pardo - SP, 18900-000",
+                                                        timeOfWork: "Aberto 24h",
+                                                        phone: "(14) 3332-1033",
+                                                        image: "graal",
+                                                        rating: 4.5,
+                                                        verified: true,
+                                                        coordinate: CLLocation(latitude: -22.8061941, longitude: -49.531047))]))
+            default:
+                completion(.success([]))
+        }
     }
 }
